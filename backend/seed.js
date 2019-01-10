4bd54457339d0d470ee13aa9fc9541f32bf206f0
Tao Chu <taochu@Taos-MacBook-Pro.local>
2019-01-10 14:03:29 +0800

    add seed.js

---
+++ backend/seed.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const seeds = require("./seeds")
+
+
+mongoose.connect('mongodb://localhost:27017/shopping-cart', {useNewUrlParser: true})
+db = mongoose.connection
+
+db.once('open', () => {
+    Object.keys(seeds).forEach(key => {
+        collection = db.collection(key)
+        seeds[key].forEach(seed => {
+            seed._id = new mongoose.Types.ObjectId(seed._id)
+            collection.updateOne(
+                { _id: seed._id },
+                {$set: seed},
+                { upsert: true, forceServerObjectId: true },
+                (err, result) => {
+                    if (err) {
+                        throw err
+                    }
+                    console.log(result)
+                }
+            )
+        })    
+    })
+})
+
+db.close()
